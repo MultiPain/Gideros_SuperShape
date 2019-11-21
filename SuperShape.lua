75e319121cc70c615cd390c0a696c148d019e892
MultiPain <32590972+MultiPain@users.noreply.github.com>
2019-11-22 00:33:13 +0900

    Add files via upload

---
+++ SuperShape.lua
@@ -0,0 +1,77 @@
+local cos,sin,abs,pi = math.cos,math.sin,math.abs,math.pi
+local pi2 = pi*2
+SuperShape = Core.class(Mesh)
+
+SuperShape.SHAPE = "Shape"
+SuperShape.ELLIPSE = "Ellipse"
+SuperShape.CIRCLE = "Circle"
+
+local function sign(v) return v < 0 and -1 or v > 0 and 1 or 0 end
+
+function SuperShape:init(type, color, total)
+	self.type = type or SuperShape.SHAPE
+	self.color = color or 0xffffff
+	self.total = total or 128
+end
+--
+function SuperShape:build(...)
+	local inc = pi2 / self.total
+
+	self:setVertex(1, 0, 0)
+	self:setColor(1, self.color, 1)
+
+	local ind = 1
+	local prev = 2
+	local i = 1
+	for j = 0, pi2, inc do 		
+		local x, y = self["build"..self.type](self, j, ...)
+		
+		self:setVertex(i+1, x, y)
+		self:setColor(i+1, self.color, 1)
+		
+		self:setIndex(ind, 1)
+		self:setIndex(ind+1, prev)
+		if (prev+1 > self.total+1) then 
+			self:setIndex(ind+2, 2)
+		else
+			self:setIndex(ind+2, prev+1)
+		end
+		
+		prev += 1
+		ind += 3
+		i+=1
+	end
+end
+--
+function SuperShape:buildShape(theta, r, a, b, m, n1, n2, n3)
+	local cosa = cos(theta)
+	local sina = sin(theta)
+	local av = theta * m / 4
+	local v = (abs(1 / a * cos(av))^n2 + abs(1 / b * sin(av))^n3)^(1 / n1)
+	local cr = 1 / v
+	
+	local x = r * cr * cosa
+	local y = r * cr * sina
+	
+	return x, y
+end
+--
+function SuperShape:buildEllipse(theta, a, b, m, n)
+	local cosa = cos(theta)
+	local sina = sin(theta)
+	
+	local x = abs(cosa)^(2/n) * a * sign(cosa)
+	local y = abs(sina)^(2/m) * b * sign(sina)
+	
+	return x, y
+end
+--
+function SuperShape:buildCircle(theta, r)
+	local cosa = cos(theta)
+	local sina = sin(theta)
+	
+	local x = r * cosa
+	local y = r * sina
+	
+	return x, y
+end
---
+++ main.lua
@@ -0,0 +1,38 @@
+app:setBackgroundColor(0x323232)
+
+local sw = app:getContentWidth()
+local sh = app:getContentHeight()
+
+local el = SuperShape.new(SuperShape.ELLIPSE, math.random(0xffffff), 200)
+el:build(100, 70, 0.4, 2)
+el:setPosition(sw / 5, sh / 2)
+stage:addChild(el)
+
+local ss = SuperShape.new(SuperShape.SHAPE, math.random(0xffffff), 200)
+ss:build(100, 1, 1, 5, 0.1,1.7,1.7)
+ss:setPosition(sw - sw / 5, sh / 2)
+stage:addChild(ss)
+
+local sc = SuperShape.new(SuperShape.CIRCLE, math.random(0xffffff), 200)
+sc:build(25)
+sc:setPosition(sw /2 , sh / 2)
+stage:addChild(sc)
+
+local v = 0
+function animate(e)
+	local dt = e.deltaTime
+	
+	v += dt
+	
+	local sinv = math.sin(v)
+	local cosv = math.cos(v)
+	local norms = (sinv+1)/2
+	local normc = (cosv+1)/2
+	
+	el:build(100, 70, norms*2, normc)
+	ss:build(100, 1, 1, 5, norms+0.2,1.7,1.7)
+	sc:build(25 + norms*25)
+end
+--
+
+stage:addEventListener("enterFrame", animate)
